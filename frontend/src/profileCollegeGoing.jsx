0576ed154a6971a4dc93750b2f03fd6c2f87fa98
Anitejjain <131655465+Anitejjain@users.noreply.github.com>
2024-03-14 13:45:44 +0530

    Update profileCollegeGoing.jsx

--- frontend/src/profileCollegeGoing.jsx
+++ frontend/src/profileCollegeGoing.jsx
@@ -1,10 +1,12 @@
 import React, {useState,useEffect} from 'react';
-import './profile.css';
+import './profileCollegeGoing.css';
 import { useSelector , useDispatch} from 'react-redux';
 import Logo from './Images/logo.jpg';
 import axios from 'axios';
+import { updateUserName ,updateEmail, updateCollege, updateBranch, updateAddress, updateGraduationYear} from "./actions/authActions";
 
 function CollegeGoingProfile(){
+
     const userData = useSelector(state => state.auth.user); 
 
     return(
@@ -47,32 +49,46 @@ function ProfileSection({userData}){
   
   function ProfileDataSection({ userData }) {
     const [isEditingName, setIsEditingName] = useState(false);
+    const [isEditingAddress, setIsEditingAddress] = useState(false);
     const [isEditingEmail, setIsEditingEmail] = useState(false);
     const [newName, setNewName] = useState(userData.name);
+    const [newAddress, setNewAddress] = useState(userData.address);
     const [newEmail, setNewEmail] = useState(userData.email);
     const dispatch = useDispatch();
-
-    useEffect(() => {
-        setNewName(userData.name);
-    }, [userData.name]);
 
     const handleNameEdit = () => {
         setIsEditingName(true);
     };
 
+    const handleAddressEdit = () => {
+        setIsEditingAddress(true);
+    };
+
     const handleEmailEdit = () => {
         setIsEditingEmail(true);
     };
-    const handleNameSubmit = () => {
 
+    const handleNameSubmit = () => {
         axios.patch(`http://localhost:3001/profile/updateName/${userData.email}`, {  name: newName })
             .then(response => {
                 console.log(response.data);
+                dispatch(updateUserName(newName)); 
                 setIsEditingName(false);
-                setNewName("");
             })
             .catch(error => {
                 console.error('Error updating name:', error);
+            });
+    };
+
+    const handleAddressSubmit = () => {
+        axios.patch(`http://localhost:3001/profile/updateAddress/${userData.email}`, { address: newAddress })
+            .then(response => {
+                console.log(response.data);
+                dispatch(updateAddress(newAddress));
+                setIsEditingAddress(false);
+            })
+            .catch(error => {
+                console.error('Error updating address:', error);
             });
     };
 
@@ -80,8 +96,8 @@ function ProfileSection({userData}){
         axios.patch(`http://localhost:3001/profile/updateEmail/${userData.email}`, { newEmail: newEmail })
             .then(response => {
                 console.log(response.data);
+                dispatch(updateEmail(newEmail));
                 setIsEditingEmail(false);
-                setNewEmail("");
             })
             .catch(error => {
                 console.error('Error updating email:', error);
@@ -111,6 +127,24 @@ function ProfileSection({userData}){
             </div>
             <div className="professional_details">
                 <div>
+                    <p className="profile_data_rows">Address</p>
+                </div>
+                <div style={{ display: 'grid', gridTemplateColumns: '1fr auto', marginBottom: "5px" }}>
+                    {isEditingAddress ? (
+                        <>
+                            <input type="text" value={newAddress} onChange={(e) => setNewAddress(e.target.value)} className="profile-data-values" />
+                            <button className="edit-button" onClick={handleAddressSubmit}>Save</button>
+                        </>
+                    ) : (
+                        <>
+                            <p className="profile-data-values">{userData.address}</p>
+                            <button className="edit-button" onClick={handleAddressEdit}>Edit</button>
+                        </>
+                    )}
+                </div>
+            </div>
+            <div className="professional_details">
+                <div>
                     <p className="profile_data_rows">Email</p>
                 </div>
                 <div style={{ display: 'grid', gridTemplateColumns: '1fr auto', marginBottom: "5px" }}>
@@ -127,48 +161,124 @@ function ProfileSection({userData}){
                     )}
                 </div>
             </div>
-            <div className="professional_details">
-                <div>
-                    <p className="profile_data_rows">Address</p>
-                </div>
-                <div style={{ display: 'grid', gridTemplateColumns: '1fr auto', marginBottom: "5px" }}>
-                    <p className="profile-data-values">Pune</p>
-                    <button className="edit-button">Edit</button>
-                </div>
-            </div>
         </div>
     );
 }
 
   function ProfessionalsSection({userData}){
+
+    const [isEditingCollege, setIsEditingCollege] = useState(false);
+    const [isEditingBranch, setIsEditingBranch] = useState(false);
+    const [isEditingGraduationYear, setIsEditingGraduationYear] = useState(false);
+    const [newCollege, setNewCollege] = useState(userData.college);
+    const [newBranch, setNewBranch] = useState(userData.major);
+    const [newGraduationYear, setNewGraduationYear] = useState(userData.graduationYear);
+
+    const dispatch = useDispatch();
+
+    const handleBranchEdit = () => {
+        setIsEditingBranch(true);
+    };
+
+    const handleCollegeEdit = () => {
+        setIsEditingCollege(true);
+    };
+
+    const handleGraduationYearEdit = () => {
+        setIsEditingGraduationYear(true);
+    };
+
+    const handleBranchSubmit = () => {
+        axios.patch(`http://localhost:3001/profile/updateBranch/${userData.email}`, { branch: newBranch })
+            .then(response => {
+                console.log(response.data);
+                dispatch(updateBranch(newBranch));
+                setIsEditingBranch(false);
+            })
+            .catch(error => {
+                console.error('Error updating branch:', error);
+            });
+    };
+
+    const handleCollegeSubmit = () => {
+        axios.patch(`http://localhost:3001/profile/updateCollege/${userData.email}`, { newCollege: newCollege })
+            .then(response => {
+                console.log(response.data);
+                dispatch(updateCollege(newCollege));
+                setIsEditingCollege(false);
+            })
+            .catch(error => {
+                console.error('Error updating college:', error);
+            });
+    };
+
+    const handleGraduationYearSubmit = () => {
+        axios.patch(`http://localhost:3001/profile/updateGraduationYear/${userData.email}`, { graduationYear: newGraduationYear })
+            .then(response => {
+                console.log(response.data);
+                dispatch(updateGraduationYear(newGraduationYear));
+                setIsEditingGraduationYear(false);
+            })
+            .catch(error => {
+                console.error('Error updating graduation year:', error);
+            });
+    };
+
     return(
         <div className="professional">
             <p style={{textAlign: 'center', fontSize: '30px', marginTop: "15px",marginBottom:"15px", fontWeight:'500'}}>College Details</p>
             <div className="professional_details">
                 <div>
-                    <p className="profile_data_rows">Currently Studying at/Graduated from</p>
-                </div>
-                <div style={{display:'grid', gridTemplateColumns:'1fr auto', marginBottom:"5px"}}>
-                    <p className="profile-data-values">{userData.college}</p>
-                    <button className="edit-button">Edit</button>
-                </div>
-            </div>
-            <div className="professional_details">
-                <div>
-                    <p className="profile_data_rows">Rate Your College</p>
-                </div>
-                <div style={{display:'grid', gridTemplateColumns:'1fr auto', marginBottom:"5px"}}>
-                    <p className="profile-data-values">4 stars</p>
-                    <button className="edit-button">Edit</button>
-                </div>
-            </div>
-            <div className="professional_details">
-                <div>
                     <p className="profile_data_rows">Your Branch</p>
                 </div>
-                <div style={{display:'grid', gridTemplateColumns:'1fr auto' , marginBottom:"5px"}}>
-                    <p className="profile-data-values">{userData.major}</p>
-                    <button className="edit-button">Edit</button>
+                <div style={{ display: 'grid', gridTemplateColumns: '1fr auto', marginBottom: "5px" }}>
+                    {isEditingBranch ? (
+                        <>
+                            <input type="text" value={newBranch} onChange={(e) => setNewBranch(e.target.value)} className="profile-data-values" />
+                            <button className="edit-button" onClick={handleBranchSubmit}>Save</button>
+                        </>
+                    ) : (
+                        <>
+                            <p className="profile-data-values">{userData.major}</p>
+                            <button className="edit-button" onClick={handleBranchEdit}>Edit</button>
+                        </>
+                    )}
+                </div>
+            </div>
+            <div className="professional_details">
+                <div>
+                    <p className="profile_data_rows">Currently Studying at/Graduated from</p>
+                </div>
+                <div style={{ display: 'grid', gridTemplateColumns: '1fr auto', marginBottom: "5px" }}>
+                    {isEditingCollege ? (
+                        <>
+                            <input type="text" value={newCollege} onChange={(e) => setNewCollege(e.target.value)} className="profile-data-values" />
+                            <button className="edit-button" onClick={handleCollegeSubmit}>Save</button>
+                        </>
+                    ) : (
+                        <>
+                            <p className="profile-data-values">{userData.college}</p>
+                            <button className="edit-button" onClick={handleCollegeEdit}>Edit</button>
+                        </>
+                    )}
+                </div>
+            </div>
+            <div className="professional_details">
+                <div>
+                    <p className="profile_data_rows">Graduation Year</p>
+                </div>
+                <div style={{ display: 'grid', gridTemplateColumns: '1fr auto', marginBottom: "5px" }}>
+                    {isEditingGraduationYear ? (
+                        <>
+                            <input type="text" value={newGraduationYear} onChange={(e) => setNewGraduationYear(e.target.value)} className="profile-data-values" />
+                            <button className="edit-button" onClick={handleGraduationYearSubmit}>Save</button>
+                        </>
+                    ) : (
+                        <>
+                            <p className="profile-data-values">{userData.graduationYear}</p>
+                            <button className="edit-button" onClick={handleGraduationYearEdit}>Edit</button>
+                        </>
+                    )}
                 </div>
             </div>
         </div>
@@ -177,41 +287,41 @@ function ProfileSection({userData}){
 
   function Opinions({userData}){
     return(
-        <div className="professional">
+        <div className="opinions">
             <p style={{textAlign: 'center', fontSize: '30px', marginTop: "15px",marginBottom:"15px", fontWeight:'500'}}>Opinions</p>
-            <div className="professional_details">
+            <div className="opinions_details">
                 <div>
-                    <p className="profile_data_rows">Academics</p>
+                    <p className="opinions_data_rows">Academics</p>
                 </div>
                 <div style={{display:'grid', gridTemplateColumns:'1fr auto', marginBottom:"5px"}}>
-                    <p className="profile-data-values">{userData.college}</p>
+                    <p className="opinions-data-values">{userData.college}</p>
                     <button className="edit-button">Edit</button>
                 </div>
             </div>
-            <div className="professional_details">
+            <div className="opinions_details">
                 <div>
-                    <p className="profile_data_rows">Extra Curricular Activities</p>
+                    <p className="opinions_data_rows">Extra Curricular Activities</p>
                 </div>
                 <div style={{display:'grid', gridTemplateColumns:'1fr auto', marginBottom:"5px"}}>
-                    <p className="profile-data-values">4 stars</p>
+                    <p className="opinions-data-values">4 stars</p>
                     <button className="edit-button">Edit</button>
                 </div>
             </div>
-            <div className="professional_details">
+            <div className="opinions_details">
                 <div>
-                    <p className="profile_data_rows">Placements</p>
+                    <p className="opinions_data_rows">Placements</p>
                 </div>
                 <div style={{display:'grid', gridTemplateColumns:'1fr auto' , marginBottom:"5px"}}>
-                    <p className="profile-data-values">It is very good</p>
+                    <p className="opinions-data-values">It is very good</p>
                     <button className="edit-button">Edit</button>
                 </div>
             </div>
-            <div className="professional_details">
+            <div className="opinions_details">
                 <div>
-                    <p className="profile_data_rows">Overall</p>
+                    <p className="opinions_data_rows">Overall</p>
                 </div>
                 <div style={{display:'grid', gridTemplateColumns:'1fr auto' , marginBottom:"5px"}}>
-                    <p className="profile-data-values">{userData.major}</p>
+                    <p className="opinions-data-values">{userData.major}</p>
                     <button className="edit-button">Edit</button>
                 </div>
             </div>
